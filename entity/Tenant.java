5673538d0accbd1afbce0a684b63f9bd8e291824
PutDownEgo <tim4v.me@gmail.com>
2023-11-11 14:41:01 +0700

    tenant

---
+++ entity/Tenant.java
@@ -0,0 +1,13 @@
+
+public class Tenant extends User{
+
+    public Tenant(String UserID, String FirstName, String LastName, String Email, String Password) {
+        super(UserID, FirstName, LastName, Email, Password);
+    }
+    
+    public void CreateRentalContract(){
+    }
+    public void TerminateRentalContract(){
+        
+    }
+}
